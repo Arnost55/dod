069f532ecc548960a6680d9932c410677487d9ef
Arnost55 <99980693+Arnost55@users.noreply.github.com>
2023-12-03 19:00:00 +0100

    Aktualizovať pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,84 +1,210 @@
-function stopCar () {
-    pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-}
-function moveForward () {
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-}
-function turnLeft () {
-    pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-}
-input.onButtonPressed(Button.A, function () {
+let autoMode = false;
+
+// Placeholder motor control pins, replace with actual pin numbers
+const MOTOR_LEFT_FORWARD = DigitalPin.P12;
+const MOTOR_LEFT_BACKWARD = DigitalPin.P8;
+const MOTOR_RIGHT_FORWARD = DigitalPin.P16;
+const MOTOR_RIGHT_BACKWARD = DigitalPin.P0;
+
+const IR_FORWARD = 0xFFA25D;
+const IR_BACKWARD = 0xFF629D;
+const IR_LEFT = 0xFFE21D;
+const IR_RIGHT = 0xFF22DD;
+const IR_STOP = 0xFF02FD;
+const IR_AB = 0xFFC23D;
+
+input.onIrButton(IR_FORWARD, function () {
     // Move forward
-    if (!(autoMode)) {
-        moveForward()
+    if (!autoMode) {
+        moveForward();
     }
 })
-function autoModeLogic () {
+
+input.onIrButton(IR_BACKWARD, function () {
+    // Move backward
+    if (!autoMode) {
+        moveBackward();
+    }
+})
+
+input.onIrButton(IR_LEFT, function () {
+    // Turn left
+    if (!autoMode) {
+        turnLeft();
+    }
+})
+
+input.onIrButton(IR_RIGHT, function () {
+    // Turn right
+    if (!autoMode) {
+        turnRight();
+    }
+})
+
+input.onIrButton(IR_STOP, function () {
+    // Stop
+    if (!autoMode) {
+        stopCar();
+    }
+})
+
+input.onIrButton(IR_AB, function () {
+    // Toggle auto mode
+    autoMode = !autoMode;
+
+    if (autoMode) {
+        basic.showIcon(IconNames.Yes);
+        autoModeLogic();
+    } else {
+        basic.clearScreen();
+        stopCar();
+    }
+})
+
+function moveForward() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function moveBackward() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 1);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 1);
+}
+
+function stopCar() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function turnLeft() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function turnRight() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function autoModeLogic() {
     basic.forever(function () {
-        // Read obstacle avoidance sensors
-        let obstacleLeft = pins.digitalReadPin(DigitalPin.P1);
-        let obstacleRight = pins.digitalReadPin(DigitalPin.P2);
-
-        // Both sensors detect an obstacle
-        if (obstacleLeft == 0 && obstacleRight == 0) {
-            // Stop and turn around
-            moveBackward();
-            basic.pause(500);
-            turnRight();
-            basic.pause(500);
-        } else if (obstacleLeft == 0) {
-            // Obstacle detected on the left, turn right
-            turnRight();
-        } else if (obstacleRight == 0) {
-            // Obstacle detected on the right, turn left
-            turnLeft();
-        } else {
-            // No obstacle, move forward
-            moveForward();
-        }
-
+        // Implement auto mode logic using IR remote
+        // You need to adjust this based on your specific sensor and motor control configuration
         basic.pause(100); // Adjust the pause time based on your car's speed and responsiveness
     });
 }
-input.onButtonPressed(Button.AB, function () {
-    // Toggle auto mode
-    autoMode = !(autoMode)
-    if (autoMode) {
-        basic.showIcon(IconNames.Yes)
-        autoModeLogic()
-    } else {
-        basic.clearScreen()
-        stopCar()
+let autoMode = false;
+
+// Placeholder motor control pins, replace with actual pin numbers
+const MOTOR_LEFT_FORWARD = DigitalPin.P12;
+const MOTOR_LEFT_BACKWARD = DigitalPin.P8;
+const MOTOR_RIGHT_FORWARD = DigitalPin.P16;
+const MOTOR_RIGHT_BACKWARD = DigitalPin.P0;
+
+const IR_FORWARD = 0xFFA25D;
+const IR_BACKWARD = 0xFF629D;
+const IR_LEFT = 0xFFE21D;
+const IR_RIGHT = 0xFF22DD;
+const IR_STOP = 0xFF02FD;
+const IR_AB = 0xFFC23D;
+
+input.onIrButton(IR_FORWARD, function () {
+    // Move forward
+    if (!autoMode) {
+        moveForward();
     }
 })
-function turnRight () {
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-}
-input.onButtonPressed(Button.B, function () {
+
+input.onIrButton(IR_BACKWARD, function () {
     // Move backward
-    if (!(autoMode)) {
-        moveBackward()
+    if (!autoMode) {
+        moveBackward();
     }
 })
-function moveBackward () {
-    pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-}
-let autoMode = false
-basic.forever(function () {
-	
+
+input.onIrButton(IR_LEFT, function () {
+    // Turn left
+    if (!autoMode) {
+        turnLeft();
+    }
 })
+
+input.onIrButton(IR_RIGHT, function () {
+    // Turn right
+    if (!autoMode) {
+        turnRight();
+    }
+})
+
+input.onIrButton(IR_STOP, function () {
+    // Stop
+    if (!autoMode) {
+        stopCar();
+    }
+})
+
+input.onIrButton(IR_AB, function () {
+    // Toggle auto mode
+    autoMode = !autoMode;
+
+    if (autoMode) {
+        basic.showIcon(IconNames.Yes);
+        autoModeLogic();
+    } else {
+        basic.clearScreen();
+        stopCar();
+    }
+})
+
+function moveForward() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function moveBackward() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 1);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 1);
+}
+
+function stopCar() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function turnLeft() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function turnRight() {
+    pins.digitalWritePin(MOTOR_LEFT_FORWARD, 1);
+    pins.digitalWritePin(MOTOR_LEFT_BACKWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_FORWARD, 0);
+    pins.digitalWritePin(MOTOR_RIGHT_BACKWARD, 0);
+}
+
+function autoModeLogic() {
+    basic.forever(function () {
+        // Implement auto mode logic using IR remote
+        // You need to adjust this based on your specific sensor and motor control configuration
+        basic.pause(100); // Adjust the pause time based on your car's speed and responsiveness
+    });
+}
